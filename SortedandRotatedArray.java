c3276e04eb22ec192211f9ddb2c8a3d90d61d505
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-08-13 07:03:16 +0000

    Sorted and rotated array

--- SortedandRotatedArray.java
+++ SortedandRotatedArray.java
@@ -1,60 +1,39 @@
 public class SortedandRotatedArray {
 
-    public static int recurr(int arr[], int si, int ei, int tar) {
-        if (si > ei) {
-            return -1; // Element not found
+    public static int index(int arr[],int si,int ei,int tar){
+        //Base Case
+        if(si>ei){
+            return -1;
         }
-        
-        int mid = (si + ei) / 2;
-        
-        if (arr[mid] == tar) {
+        //Working
+        int mid=(si+ei)/2;
+        //case found
+        if(arr[mid]==tar){
             return mid;
         }
-        
-        int idx = -1; // Initialize idx with a default value
-        if (arr[si] <= arr[mid]) {
-            if (arr[si] <= tar && tar <= arr[mid]) {
-                idx = recurr(arr, si, mid - 1, tar);
-            } else {
-                idx = recurr(arr, mid + 1, ei, tar);
+        //mid on L1
+        if(arr[si]<=arr[mid]){
+            //case a: left
+            if(arr[si]<=tar && tar<=arr[mid]){
+                return index(arr,si,mid,tar);
+            }else{
+                //Cases b=right
+                return index(arr,mid+1,ei,tar);
             }
-        } else {
-            if (arr[mid] <= tar && tar <= arr[ei]) {
-                idx = recurr(arr, mid + 1, ei, tar);
-            } else {
-                idx = recurr(arr, si, mid - 1, tar);
+        }else{
+            //mid on L2
+            //case c: Right
+            if(arr[mid]<=tar&&tar<=arr[ei]){
+                return index(arr,mid+1,ei,tar);
+            }else{
+                //left
+                return index(arr,si,mid-1,tar);
             }
         }
-        
-        return idx; // Return the index
     }
-
-    public static int index(int arr[], int si, int ei, int tar) {
-        int mid = (si + ei) / 2;
-        while (si <= ei) {
-            if (arr[mid] == tar) {
-                return mid;
-            } else if (arr[si] <= arr[mid]) {
-                if (arr[si] <= tar && tar <= arr[mid]) {
-                    ei = mid - 1;
-                } else {
-                    si = mid + 1;
-                }
-            } else {
-                if (arr[mid] <= tar && tar <= arr[ei]) {
-                    si = mid + 1;
-                } else {
-                    ei = mid - 1;
-                }
-            }
-            mid = (si + ei) / 2;
-        }
-        return -1; // Element not found
-    }
-
     public static void main(String args[]) {
         int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
-        int tar = 0;
+        int tar = 4;
         System.out.println(index(arr, 0, arr.length - 1, tar));
     }
 }
